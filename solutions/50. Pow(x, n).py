96b90ac9915407d849fde4abb275e10e40130cdf
blueskies0038 <67013019+blueskies0038@users.noreply.github.com>
2020-07-01 10:02:05 -0400

    28 ms

---
+++ solutions/50. Pow(x, n).py
@@ -0,0 +1,10 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        n = abs(n)
+        result = 1
+        while True:
+            
