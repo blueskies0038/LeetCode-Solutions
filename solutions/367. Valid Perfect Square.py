e9ba95a7301a2c62b73695ea5a3eb185cb5d9b63
blueskies0038 <67013019+blueskies0038@users.noreply.github.com>
2020-10-11 14:39:55 -0400

    24 ms

---
+++ solutions/367. Valid Perfect Square.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        start = 1
+        end = num + 1
+        while end >= start:
+            mid = (start + end)//2
+            if mid**2 == num:
+                return True
+            elif mid**2 > num:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return False
