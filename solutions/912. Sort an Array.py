b1b6d6d3033531bbe50a04b1c4d5e9b559ec52cc
blueskies0038 <67013019+blueskies0038@users.noreply.github.com>
2020-12-23 14:38:09 -0500

    260 ms

---
+++ solutions/912. Sort an Array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if not nums or len(nums) == 0:
+            return nums
+        self.quick_sort(nums, 0, len(nums)-1)
+        return nums
+​
+    def quick_sort(self, nums, start, end):
+        if start >= end:
+            return 
+​
+        left, right = start, end
+        mid = left + (right - left) // 2
+        pivot = nums[mid]
+​
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1
+            while left <= right and nums[right] > pivot:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+​
+        self.quick_sort(nums, start, right)
+        self.quick_sort(nums, left, end)
