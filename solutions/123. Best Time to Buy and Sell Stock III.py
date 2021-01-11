09b8c4cf56995e54f1fe70b77790752fcb9ad485
blueskies0038 <67013019+blueskies0038@users.noreply.github.com>
2021-01-11 13:56:02 -0500

    Update 123. Best Time to Buy and Sell Stock III.py

--- solutions/123. Best Time to Buy and Sell Stock III.py
+++ solutions/123. Best Time to Buy and Sell Stock III.py
@@ -1,10 +1,14 @@
+# LeetCode Problem 123. Best Time to Buy and Sell Stock III (Python)
+# Given an array for which the ith element is the price of a given stock on day i,
+# return the maximum profit after at most two transactions
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         def maxProfit(self, prices: List[int]) -> int:
         if not prices:
             return 0
 ​
-        # Forward Traversal: profits record the max profit by the ith day
+        # Forward Traversal: profits record the max profit by the ith day (transaction 1)
         profits = []
         max_profit = 0
         current_min = prices[0]
@@ -13,7 +17,7 @@ class Solution:
             max_profit = max(max_profit, price - current_min)
             profits.append(max_profit)
 ​
-        # Backward Traversal: max_profit records the max profit after the ith day
+        # Backward Traversal: max_profit records the max profit after the ith day (transaction 2)
         total_max = 0    
         max_profit = 0
         current_max = prices[-1]
