587a84ea55487d2e83d232594864d76a7c4affb6
blueskies0038 <67013019+blueskies0038@users.noreply.github.com>
2021-01-26 13:11:51 -0500

    452 ms

---
+++ solutions/313. Super Ugly Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        size = len(primes)
+        ugly, dp, index, ugly_nums = 1, [1], [0] * size, [1] * size
+        
+        for i in range(1, n):
+            for j in range(0, size):
+                if ugly_nums[j] == ugly:
+                    ugly_nums[j] = dp[index[j]] * primes[j]
+                    index[j] += 1
+            ugly = min(ugly_nums)
+            dp.append(ugly)
+            
+        return dp[-1]
