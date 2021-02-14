b8c7a5915ac945ec2ecd69ff9ca6b24c94f2fd04
blueskies0038 <67013019+blueskies0038@users.noreply.github.com>
2021-02-14 18:11:00 -0500

    40 ms

---
+++ solutions/840. Magic Squares In Grid.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        output = 0
+        for i in range(len(grid)-2):
+            for j in range(len(grid)-2):
+                temp = [grid[i+k][j:j+3] for k in range(3)]
+                if self.isMagicSquare(temp):
+                    output += 1
+        return output
+                    
+    def isMagicSquare(self, grid):
+        flat = [num for row in grid for num in row]
+        if sorted(flat) != [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            return False
+        
+        row_sums = [sum(row) for row in grid]
+        col_sums = [sum([row[i] for row in grid]) for i in range(3)]
+        diag_sums = [sum([grid[i][i] for i in range(3)]), (grid[0][2] + grid[1][1] + grid[2][0])]
+        row_sums.extend(col_sums)
+        row_sums.extend(diag_sums)
+        return len(set(row_sums)) == 1
