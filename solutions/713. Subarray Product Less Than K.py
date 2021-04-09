3319c328825a5a367bf4fa5472090bf8e9d49566
blueskies0038 <67013019+blueskies0038@users.noreply.github.com>
2021-04-09 16:59:41 -0400

    1092 ms

---
+++ solutions/713. Subarray Product Less Than K.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        output = 0
+        start = 0
+        product = 1
+        
+        for end in range(len(nums)):
+            product *= nums[end]
+            while product >= k and start <= end:
+                product /= nums[start]
+                start += 1
+            output += end - start + 1
+        
+        return output
