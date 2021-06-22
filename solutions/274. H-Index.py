2a58a4b4baf26ce992a14ae0fdb86a825067da21
Jenny Bao <67013019+blueskies0038@users.noreply.github.com>
2021-06-22 17:52:23 -0400

    32 ms

---
+++ solutions/274. H-Index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort()
+        
+        for i in range(n):
+            if citations[i] >= (n-i):
+                return n - i
+            
+        return 0
